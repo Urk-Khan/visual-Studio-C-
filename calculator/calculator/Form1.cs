3dca55425cc7769a410c2647946b73ca35c2d46c
UR Khan <urk.khan@outlook.com>
2022-02-27 23:58:43 +0500

    first commit of calculator

---
+++ calculator/calculator/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            if(textBox.Text=="0")
+                textBox.Clear();
+            Button button = (Button)sender;
+            textBox.Text = textBox.Text + button.Text;
+        }
+    }
+}
